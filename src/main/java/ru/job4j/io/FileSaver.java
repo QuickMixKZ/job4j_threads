e86d40af0bbed57f94c9bb03d84d8e5884a70e8a
QuickMixKZ <quickmixkz@gmail.com>
2022-07-03 13:28:32 +0500

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/FileSaver.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public final class FileSaver {
+
+    private final File file;
+
+    public FileSaver(File file) {
+        this.file = file;
+    }
+
+    public void saveContent(String content) {
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)))) {
+            for (int i = 0; i < content.length(); i += 1) {
+                writer.write(content.charAt(i));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public final class ParseFile {
+
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public String getContent(Predicate<Character> filter) {
+        StringBuilder output = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+            int data;
+            while ((data = reader.read()) > 0) {
+                if (filter.test((char) data)) {
+                    output.append((char) data);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+}
