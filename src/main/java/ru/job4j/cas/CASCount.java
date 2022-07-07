fff7fb9f22b0750180b8f65cb9aba5323776bf2e
QuickMixKZ <quickmixkz@gmail.com>
2022-07-07 12:24:24 +0500

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -0,0 +1,23 @@
+package ru.job4j.cas;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public void increment() {
+        int current;
+        do {
+            current = get();
+        } while(!count.compareAndSet(current, current + 1));
+    }
+
+    public int get() {
+        return count.get();
+    }
+
+}
