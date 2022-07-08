588ae1ae282649515e3079570928fb24f4045f28
QuickMixKZ <quickmixkz@gmail.com>
2022-07-08 12:29:01 +0500

    2. ExecutorService рассылка почты. [#63097 #319985]

--- src/main/java/ru/job4j/pool/EmailNotification.java
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Executors;
 
 public class EmailNotification {
 
-    ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public void emailTo(User user) {
         String subject = String.format("Notification %s to email %s", user.getUsername(), user.getEmail());
