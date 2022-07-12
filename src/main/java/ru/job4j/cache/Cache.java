38903a762ac67545080da59accac66767f6acb7a
QuickMixKZ <quickmixkz@gmail.com>
2022-07-12 12:01:44 +0500

    1. Ветик

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,28 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        return memory.computeIfPresent(model.getId(), (id, stored) -> {
+            if (stored.getVersion() != model.getVersion()) {
+                throw new OptimisticException("Versions are not equal");
+            }
+            stored = model;
+
+            return stored;
+        }) != null;
+    }
+
+    public void delete(Base model) {
+        memory.remove(model.getId(), model);
+    }
+}
