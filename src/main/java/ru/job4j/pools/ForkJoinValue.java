0d7b099c16cee0dad866ff7d8f796f6a288887d8
QuickMixKZ <quickmixkz@gmail.com>
2022-07-12 12:31:07 +0500

    1.1. Ветки

--- src/main/java/ru/job4j/pools/ForkJoinValue.java
+++ src/main/java/ru/job4j/pools/ForkJoinValue.java
@@ -1,2 +1,46 @@
-package ru.job4j.pools;public class ForkJoinValue {
+package ru.job4j.pools;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class ForkJoinValue<T> extends RecursiveTask<Integer> {
+
+    private T value;
+    private T[] array;
+    private final int lengthForLinear = 10;
+
+    public ForkJoinValue(T value, T[] array) {
+        this.value = value;
+        this.array = array;
+    }
+
+    @Override
+    protected Integer compute() {
+        int result = -1;
+        if (array.length <= lengthForLinear) {
+            result = computeLinear();
+        }
+
+        return result;
+    }
+
+    private Integer computeLinear() {
+        int result = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(value)) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int value = 10;
+        Integer[] array = new Integer[100];
+        for (int i = 0; i < 100; i++) {
+            array[i] = i;
+        }
+        int index = new ForkJoinPool().invoke(new ForkJoinValue<>(value, array));
+    }
 }
